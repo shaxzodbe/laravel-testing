e6ac87234d0441fd7344086c125b077cf1c72680
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-04-02 18:28:47 +0500

    Product added with currency convert service

---
+++ tests/Unit/CurrencyTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\CurrencyService;
+use PHPUnit\Framework\TestCase;
+
+class CurrencyTest extends TestCase
+{
+    public function test_convert_usd_to_eur_successful()
+    {
+        $this->assertEquals(91, (new CurrencyService())->convert(100, 'usd', 'eur'));
+    }
+}
