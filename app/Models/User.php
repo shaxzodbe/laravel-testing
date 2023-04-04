f516af967166ae21a356a67d9f2e015be6a625de
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-04-04 12:28:12 +0500

    Added accessor and mutator to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Enum\ProjectLevel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use function Pest\Laravel\get;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -40,6 +43,26 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'email_verified_at' => 'date:m/d/Y',
+        'project_level' => ProjectLevel::class
     ];
+
+    public function createdDiff(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->created_at->diffForHumans()
+        );
+    }
+
+    public function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => ucfirst($value)
+        );
+    }
+
+    public function getCreatedDiffAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
