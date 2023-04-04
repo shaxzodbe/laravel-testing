410d428e6a6f3d12b5c35b3268f38312b41bb113
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-04-04 09:37:14 +0500

    Model should be strict added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::shouldBeStrict(!app()->environment('production'));
     }
 }
