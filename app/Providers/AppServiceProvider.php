f696d8f5dc92154cec3c51f006f7dab25e584915
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-04-11 12:58:57 +0500

    Lazy loading restricted/prevented

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::preventLazyLoading(!$this->app->isProduction());
         Model::shouldBeStrict(!app()->environment('production'));
     }
 }
